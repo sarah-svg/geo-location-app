e9239d2c6171f0f8254b9f16c03d220ad37e81ed
sarah-svg <kjskillen@gmail.com>
2022-02-11 23:28:12 -0800

    took locate out and moved to its own file

---
+++ src/LocateUserAndRenderLocation.jsx
@@ -0,0 +1,22 @@
+export default function LocateUserAndRenderLocation({ panTo }) {
+    ///// takes in a callback prop of panTo. Gets the geolocation of that user and then uses the callback
+    //// to get the current location of the user and then renders the location on the map
+  return (
+  
+    <div>
+      <button className='locate-user' onClick={() => {
+        navigator.geolocation.getCurrentPosition((position) => {
+          console.log('got location validation', position);
+          panTo({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          });
+  
+        }, () => null);
+      }}>
+        <span role='img' aria-label='world'>🧭</span>
+          Click me to find your location
+      </button>
+    </div>
+  );
+}
--- src/Map.js
+++ src/Map.js
@@ -14,6 +14,7 @@ import style from './style.js';
 import './styles.css';
 
 import Search from './Search.jsx';
+import LocateUserAndRenderLocation from './LocateUserAndRenderLocation.jsx';
 
 export default function Map() {
   const [marker, setMarker] = useState([]);
@@ -127,31 +128,6 @@ export default function Map() {
     </div>
   );
 }
-
-function LocateUserAndRenderLocation({ panTo }) {
-  ///// takes in a callback prop of panTo. Gets the geolocation of that user and then uses the callback
-  //// to get the current location of the user and then renders the location on the map
-  return (
-
-    <div>
-      <button className='locate-user' onClick={() => {
-        navigator.geolocation.getCurrentPosition((position) => {
-          console.log('got location validation', position);
-          panTo({
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          });
-
-        }, () => null);
-      }}>
-        <span role='img' aria-label='world'>🧭</span>
-        Click me to find your location
-      </button>
-    </div>
-  );
-}
-
-
 
 
 
